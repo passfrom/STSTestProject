95a281bbae0f6c1fa0b8d45dada0e00c267df74e
yss <yss@yss-PC>
2018-04-09 13:48:07 +0800

    test

--- GitTest/src/com/wh/java/GitClass.java
+++ GitTest/src/com/wh/java/GitClass.java
@@ -6,6 +6,7 @@ public class GitClass {
 		// TODO Auto-generated method stub
 		System.out.println("Just show something!!");
 		System.out.println("Just show something other!!");
+		System.out.println("Test commit");
 	}
 
 }
