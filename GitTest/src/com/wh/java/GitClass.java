61244250f786096e02e7de869581746398ed7c61
yss <yss@yss-PC>
2018-04-09 13:41:58 +0800

    提交

---
+++ GitTest/src/com/wh/java/GitClass.java
@@ -0,0 +1,11 @@
+package com.wh.java;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Just show something!!");
+		System.out.println("Just show something other!!");
+	}
+
+}
